39b73239b676e9bcf7998e44ac5e43849100bee2
Messlow <github.pummel909@passmail.net>
2025-10-18 09:31:02 -0400

    switch qwerty

--- keyboards/sofle_choc/keymaps/Messlow/keymap.c
+++ keyboards/sofle_choc/keymaps/Messlow/keymap.c
@@ -20,7 +20,8 @@
 
 // TODO: Add dead keys FR ([], {} <>, ç, `, é)
 enum layer_names {
-    BASE_COLEMAK_LAYER,
+    BASE_QWERTY_LAYER,
+    // BASE_COLEMAK_LAYER,
     NAVIGATION_LAYER,
     TRANSPARENT_LAYER,
 };
@@ -28,27 +29,51 @@ enum layer_names {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * COLEMAK
+     * QWERTY
      * ,-----------------------------------------.                    ,-----------------------------------------.
      * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+     * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | Tab  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+     * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
      * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
-     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
      *            | LCTL | LGUI | LCMD | LALT | /Enter  /       \Space \  | RALT | RCMD | RGUI | RCTL |
      *            |      |      |      |      |/       /         \      \ |      |      |      |      |
      *            `----------------------------------'           '------''---------------------------'
      */
-    [BASE_COLEMAK_LAYER] = LAYOUT(
+
+    [BASE_QWERTY_LAYER] = LAYOUT(
         KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_RGUI,
-        KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                       KC_J,     KC_L,     KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                       KC_H,     KC_N,     KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,   MS_BTN3,KC_K,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LCTL, KC_LALT, TT(NAVIGATION_LAYER), KC_DEL, KC_ENT,          KC_SPC, KC_BSPC,  NAV_ESC,  KC_RALT, KC_RCTL
+        KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,   MS_BTN3,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                        KC_LCTL, KC_LALT, TT(NAVIGATION_LAYER), KC_ENT, KC_DEL,    KC_BSPC, KC_SPC,  NAV_ESC,  KC_RALT, KC_RCTL
     ),
+
+    // /*
+    //  * COLEMAK
+    //  * ,-----------------------------------------.                    ,-----------------------------------------.
+    //  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+    //  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    //  * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+    //  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    //  * | Tab  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+    //  * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
+    //  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+    //  * `-----------------------------------------/       /     \      \-----------------------------------------'
+    //  *            | LCTL | LGUI | LCMD | LALT | /Enter  /       \Space \  | RALT | RCMD | RGUI | RCTL |
+    //  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+    //  *            `----------------------------------'           '------''---------------------------'
+    //  */
+    // [BASE_COLEMAK_LAYER] = LAYOUT(
+    //     KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_RGUI,
+    //     KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                       KC_J,     KC_L,     KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    //     KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                       KC_H,     KC_N,     KC_E,    KC_I,    KC_O,    KC_QUOT,
+    //     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,   MS_BTN3,KC_K,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    //                     KC_LCTL, KC_LALT, TT(NAVIGATION_LAYER), KC_DEL, KC_ENT,          KC_SPC, KC_BSPC,  NAV_ESC,  KC_RALT, KC_RCTL
+    // ),
 
     /*
      * NAVIGATION
@@ -86,9 +111,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [BASE_COLEMAK_LAYER] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
-    [NAVIGATION_LAYER]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
-    [TRANSPARENT_LAYER]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
+    [BASE_QWERTY_LAYER] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
+    // [BASE_COLEMAK_LAYER] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
+    [NAVIGATION_LAYER]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
+    [TRANSPARENT_LAYER] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
 };
 #endif
 
@@ -102,5 +128,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 void keyboard_post_init_user(void) {
     // Call the post init code.
-    set_single_persistent_default_layer(BASE_COLEMAK_LAYER);
+    set_single_persistent_default_layer(BASE_QWERTY_LAYER);
+    // set_single_persistent_default_layer(BASE_COLEMAK_LAYER);
 }
